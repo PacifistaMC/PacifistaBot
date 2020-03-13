e3624b4293bb20e4dfe0f94fc762c7c6af0b91d6
Antoine PRONNIER <antoine.pronnier@epitech.eu>
2020-03-13 19:52:51 +0100

    adding console cmds

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const DiscordInstance = require('./modules/DiscordClient');
+const ConsoleCmdsInstance = require('./modules/ConsoleCommands');
 
 const bot = new DiscordInstance();
+const consoleCommands = new ConsoleCmdsInstance();
