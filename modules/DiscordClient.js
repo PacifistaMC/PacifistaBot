e57b946fe13d8b3ed3e4d1173ad5e9c94ca9328d
Antoine PRONNIER <antoine.pronnier@epitech.eu>
2020-03-13 22:07:40 +0100

    debug log

--- modules/DiscordClient.js
+++ modules/DiscordClient.js
@@ -27,7 +27,7 @@ class DiscordClient {
         text = text.replace(/\s\s+/g, ' ');
         text = text.toLowerCase();
         let args = text.split(' ');
-        console.log(args[0].charAt(0));
+        console.log("|" + args[0].charAt(0) + "|");
         if (args[0].charAt(0) === config.defaults.prefix) {
             let cmd = args[0].substr(1);
             args.shift();
