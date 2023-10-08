afd3b0d505ec74afd4449453370a92dceb9ec05d
iGameCreep <gamecreepbs@gmail.com>
2023-10-08 13:41:42 +0200

    Rename + added ticket subject

---
+++ discord-bot/src/main/java/fr/pacifista/bot/discord/events/TicketInteractions.java
@@ -0,0 +1,94 @@
+package fr.pacifista.bot.discord.events;
+
+import fr.pacifista.api.support.tickets.client.dtos.PacifistaSupportTicketDTO;
+import fr.pacifista.api.support.tickets.client.enums.TicketCreationSource;
+import fr.pacifista.api.support.tickets.client.enums.TicketType;
+import fr.pacifista.bot.discord.api.PacifistaTicketClient;
+import fr.pacifista.bot.discord.config.Config;
+import fr.pacifista.bot.discord.events.buttons.TicketClose;
+import fr.pacifista.bot.discord.events.buttons.TicketCreate;
+import fr.pacifista.bot.discord.utils.TicketUtils;
+import lombok.NonNull;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.text.TextInput;
+import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
+import net.dv8tion.jda.api.interactions.modals.Modal;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TicketInteractions extends ListenerAdapter {
+    private final Config botConfig;
+
+    public TicketInteractions(Config botConfig) {
+        this.botConfig = botConfig;
+    }
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+        final String buttonId = event.getInteraction().getComponentId();
+
+        switch (buttonId) {
+            case "ticket-create":
+                new TicketCreate().onButton(event);
+                break;
+            case "ticket-close":
+                new TicketClose(this.botConfig).onButton(event);
+                break;
+        }
+    }
+
+    @Override
+    public void onStringSelectInteraction(@NonNull StringSelectInteractionEvent event) {
+        String selectId = event.getInteraction().getComponentId();
+        String ticketType = event.getValues().get(0);
+
+        TextInput object = TextInput.create("object", "Objet", TextInputStyle.SHORT)
+                .setPlaceholder("Objet du ticket")
+                .setMinLength(10)
+                .setMaxLength(100)
+                .setRequired(true)
+                .build();
+
+        if (selectId.equals("ticket-create")) {
+            Modal modal = Modal.create(String.format("ticket-create_%s", ticketType), "Crée un ticket")
+                    .addActionRow(object)
+                    .build();
+
+            event.replyModal(modal).queue();
+        }
+    }
+
+    @Override
+    public void onModalInteraction(@NotNull ModalInteractionEvent event) {
+        PacifistaTicketClient ticketClient = new PacifistaTicketClient(this.botConfig);
+        TicketUtils ticketUtils = new TicketUtils(event.getJDA(), this.botConfig);
+        String interactionId = event.getModalId();
+        String modalId = interactionId.split("_")[0];
+        String arg = interactionId.split("_")[1];
+
+        User user = event.getUser();
+
+        if (modalId.equals("ticket-create")) {
+            TicketType ticketType = TicketType.valueOf(arg.toUpperCase());
+            String object = event.getValue("object").getAsString();
+
+            PacifistaSupportTicketDTO ticketDTO = new PacifistaSupportTicketDTO();
+            ticketDTO.setCreatedAt(new Date());
+            ticketDTO.setCreatedById(user.getId());
+            ticketDTO.setCreatedByName(user.getName());
+            ticketDTO.setCreationSource(TicketCreationSource.DISCORD);
+            ticketDTO.setType(ticketType);
+            ticketDTO.setObject(object);
+
+            ticketClient.create(ticketDTO);
+            ticketUtils.createTicket(event, ticketType);
+        }
+    }
+}
--- discord-bot/src/main/java/fr/pacifista/bot/discord/utils/TicketUtils.java
+++ discord-bot/src/main/java/fr/pacifista/bot/discord/utils/TicketUtils.java
@@ -1,11 +1,20 @@
 package fr.pacifista.bot.discord.utils;
 
+import fr.pacifista.api.support.tickets.client.enums.TicketType;
 import fr.pacifista.bot.discord.config.Config;
+import lombok.NonNull;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.channel.concrete.Category;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.springframework.stereotype.Service;
+
+import java.util.EnumSet;
 
 @Service
 public class TicketUtils {
@@ -30,5 +39,32 @@ public class TicketUtils {
         Button button = Button.primary("ticket-create", "Créer un ticket");
 
         channel.sendMessageEmbeds(embed.build()).addActionRow(button).queue();
+    }
+
+    public void createTicket(@NonNull ModalInteractionEvent event, TicketType ticketType) {
+        Category category = event.getJDA().getCategoryById(this.config.getTicketsCategoryId());
+        TextChannel ticketChannel = category.createTextChannel(String.format("ticket-%s", event.getUser().getGlobalName())).complete();
+
+        Role modRole = event.getJDA().getRoleById(this.config.getTicketsModRoleID());
+        Role everyoneRole = event.getGuild().getRolesByName("@everyone", true).get(0);
+
+        ticketChannel.getManager()
+                .putPermissionOverride(event.getMember(), EnumSet.of(Permission.VIEW_CHANNEL, Permission.MESSAGE_SEND), null)
+                .putPermissionOverride(modRole, EnumSet.of(Permission.VIEW_CHANNEL, Permission.MESSAGE_SEND), null)
+                .putPermissionOverride(everyoneRole, null, EnumSet.of(Permission.VIEW_CHANNEL, Permission.MESSAGE_SEND))
+                .queue();
+
+        event.editMessage(String.format("Ticket crée avec succès ! <#%s>", ticketChannel.getId()))
+                .setComponents()
+                .queue();
+
+        EmbedBuilder embed = new EmbedBuilder()
+                .setColor(Colors.PACIFISTA_COLOR)
+                .setTitle(String.format("Ticket de %s (%s)", event.getMember().getUser().getEffectiveName(), event.getMember().getUser().getName()))
+                .setDescription("Ton ticket à été crée. Merci de patienter, un modérateur viendra y répondre rapidement.")
+                .addField(new MessageEmbed.Field("Type", ticketType.name(), true));
+
+        ticketChannel.sendMessageEmbeds(embed.build())
+                .queue();
     }
 }
