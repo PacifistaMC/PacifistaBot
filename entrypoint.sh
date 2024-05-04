2af035a589b0d3cd08ae3fe7a35a8c43ca7ab3e4
FunixG <antoine.pronnier@gmail.com>
2024-05-04 11:29:20 +0200

    try to handle auto updater and ^C

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ cp /home/java/pacifistabot.jar pacifistabot.jar
 echo "Java version"
 java --version
 
-java -jar -Xms150M -XX:MaxRAMPercentage=95.0 /home/java/pacifistabot.jar
+exec java -jar -Xms150M -XX:MaxRAMPercentage=95.0 /home/java/pacifistabot.jar
